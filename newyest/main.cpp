74f7d2887ca99f798f191cb815e7986cb0380844
Bogdanok98 <bogdanov.kirill@outlook.com>
2024-04-21 19:03:55 -0400

    Final Test

--- newyest/main.cpp
+++ newyest/main.cpp
@@ -7,8 +7,13 @@
 
 #include <iostream>
 #include <vector>
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string enter;
+    cin >> enter;
+    cout << enter << endl;
+    
     return 0;
 }
