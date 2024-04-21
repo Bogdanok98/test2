f0cc10b08a1b75c7e4c7cc26d18ffa6c4818f8a5
Bogdanok98 <bogdanov.kirill@outlook.com>
2024-04-21 18:58:47 -0400

    Initial Commit

---
+++ newyest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newyest
+//
+//  Created by Kirill Bogdanov on 4/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
